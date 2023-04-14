79e1e4aa7301e5900e5c4f210aebbbacf0efdd67
Alberto Harka <alb.harka@icloud.com>
2023-04-14 16:57:29 +0200

    cleanup

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -21,19 +21,21 @@ const InfoBox: React.FC<{ className?: string, children: string }> = ({ className
                     <p>{infoOpen ? '🔼' : 'ℹ️'}</p>
                 </button>
             </div>
-            {infoOpen && <ul className='px-2 pt-2 pb-1 -mt-1 bg-cyan-100 shadow-md z-50 relative rounded-b-md rounded-tl-md'>
-                <Info border>➕ and ⏎ key add a todo</Info>
-                <Info border>🖱️ click on a todo to mark it as completed</Info>
-                <Info border>➰ drag and drop to reorder</Info>
-                <Info border>✖️ deletes a todo</Info>
-                <Info border>🔎 filters todos by text and allows for exact match with double quotes </Info>
-                <Info border>✔️ toggles completed todo visibility</Info>
-                <Info border>🕣 sorts todo by order of insertion *</Info>
-                <Info border>🔀 sorts todo by completition status *</Info>
-                <Info border>🧹 clears all completed todos</Info>
-                <Info border>🗑️ deletes all todos</Info>
-                <Info>* the order gets reversed if the list is already sorted</Info>
-            </ul>}
+            { infoOpen && 
+                <ul className='px-2 pt-2 pb-1 -mt-1 bg-cyan-100 shadow-md z-50 relative rounded-b-md rounded-tl-md'>
+                    <Info border>➕ and ⏎ key add a todo</Info>
+                    <Info border>🖱️ click on a todo to mark it as completed</Info>
+                    <Info border>➰ drag and drop to reorder</Info>
+                    <Info border>✖️ deletes a todo</Info>
+                    <Info border>🔎 filters todos by text and allows for exact match with double quotes </Info>
+                    <Info border>✔️ toggles completed todo visibility</Info>
+                    <Info border>🕣 sorts todo by order of insertion *</Info>
+                    <Info border>🔀 sorts todo by completition status *</Info>
+                    <Info border>🧹 clears all completed todos</Info>
+                    <Info border>🗑️ deletes all todos</Info>
+                    <Info>* the order gets reversed if the list is already sorted</Info>
+                </ul>
+            }
         </div>
     )
 }
