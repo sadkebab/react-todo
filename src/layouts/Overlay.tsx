fdfdd67c3a287dbb9804814bdb2301d9af95682c
Alberto Harka <alb.harka@icloud.com>
2023-04-17 22:16:20 +0200

    overlay padding

--- src/layouts/Overlay.tsx
+++ src/layouts/Overlay.tsx
@@ -12,7 +12,7 @@ const Overlay: React.FC<LigtboxProps> = ({ title, children, onClose }) => {
   }
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={onClose}>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4" onClick={onClose}>
       <div
         className="rounded-md shadow-lg p-4 px-2 pt-2 pb-1 bg-gradient-to-br from-slate-800 to-slate-900 border border-slate-600 text-slate-200"
         onClick={stopBubble}
